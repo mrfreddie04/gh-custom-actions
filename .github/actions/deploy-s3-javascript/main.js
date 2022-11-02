61e8d963cf66174f5e083a10e2d7cffe29752317
mrfreddie04 <58453957+mrfreddie04@users.noreply.github.com>
2022-11-02 00:43:29 -0400

    Update 'deploy-s3-javascript' to deploy react app to AWS S3 bucket

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,28 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
-  core.notice("Hello from custom js action");
+  //1) Get input values - information about deployment target
+  //required throws an error if not input is provided
+  const bucket = core.getInput("bucket", {
+    required: true
+  });
+  const bucketRegion = core.getInput("bucket-region", {
+    required: true
+  });
+  const distFolder = core.getInput("dist-folder", {
+    required: true
+  });
+
+  //github.getOctokit() - use to make requests to GA REST API
+  //github.context - access to some context objects
+
+  //2) Upload deployment files to AWS S3
+  //- we can use AWS SDK
+  //- we can use exec package to run a cmd command, ubuntu image contains AWS CLI
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  core.notice("Website deployed to AWS S3");
 }
 
 run();
