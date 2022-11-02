c8b95baa5eca33d0ad4212465b7070f60d0c0b40
mrfreddie04 <58453957+mrfreddie04@users.noreply.github.com>
2022-11-01 23:35:47 -0400

    Add js action 'deploy-s3-javascript' - fix error - take #2

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,7 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
-  core.notice("Hello from custoom js action");
+  core.notice("Hello from custom js action");
 }
 
 run();
